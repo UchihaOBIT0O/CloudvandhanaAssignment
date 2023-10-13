5ede888df5d0693321027adb3f8b0ff510ee5883
DONTHULA ARUN <80624339+UchihaOBIT0O@users.noreply.github.com>
2023-10-13 16:09:16 +0530

    Update script.js

--- Javascript/sortingArray/script.js
+++ Javascript/sortingArray/script.js
@@ -1,1 +1,6 @@
-C:\Users\Donthula Arun\Downloads\Telegram Desktop\sortingArray
+"use strict";
+
+const arr = [1, 2, 3, 9, 8, 7, 4, 6, 5];
+const descending = (arr) => arr.sort((a, b) => b - a);
+document.getElementById("arr").textContent += `${arr}`;
+document.getElementById("span").textContent += descending(arr);
